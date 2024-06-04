3fd3a48af7a1ddaedc3de569ba0683784a770ba6
Masooma Zia <masoomamasooma1082@gmail.com>
2024-06-05 01:08:26 +0500

    modify

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -19,7 +19,7 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/last.jpeg";
+import image from "../images/last.png";
 
 const imageAltText = "portfolio_aesthetic";
 
